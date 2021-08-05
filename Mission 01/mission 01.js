90d5486b844a070d92231abdcf6974cfda63df4c
최연희 <yeonhee756@gmail.com>
2021-08-05 23:56:49 +0900

    Search, Darkmode 구현

--- Mission 01/mission 01.js
+++ Mission 01/mission 01.js
@@ -6,12 +6,13 @@ function DarkMode() {
     body.classList.toggle("dark-mode");
 
     var button = document.getElementById("dark_bt");
+
     if (button.innerHTML == "다크 모드로 보기") {
         button.innerHTML = "라이트 모드로 보기";
         button.style.backgroundColor = "#FFEFB2";
     } else {
         button.innerHTML = "다크 모드로 보기";
-        button.style.backgroundColor = "#E3DBEB";
+        button.style.backgroundColor = "#8977ad";
     }
 }
 
